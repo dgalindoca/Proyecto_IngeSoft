3c0a916a9f09c3df3086ac91b93788bc5c44c5fd
Daniel <dgalindoca@unal.edu.co>
2022-01-15 10:37:24 -0500

    prueba

--- src/main/java/com/unal/lizzard/web/MainController.java
+++ src/main/java/com/unal/lizzard/web/MainController.java
@@ -3,6 +3,7 @@ package com.unal.lizzard.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //prueba
+//prueba2
 @Controller
 public class MainController {
     @GetMapping("/login")
